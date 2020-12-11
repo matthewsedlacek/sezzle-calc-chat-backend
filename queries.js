d0935eeb8c10410e56e24e15b7614054191d83b5
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-12-11 13:20:42 -0800

    added get Messages and create message functions in queries.js

--- queries.js
+++ queries.js
@@ -6,3 +6,32 @@ const pool = new Pool({
   password: "password",
   port: 5432,
 });
+
+const getMessages = (request, response) => {
+  pool.query("SELECT * FROM messages ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createMessage = (request, response) => {
+  const { text, username } = request.body;
+
+  pool.query(
+    "INSERT INTO messages (text, username) VALUES ($1, $2)",
+    [text, username],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${result.insertId}`);
+    }
+  );
+};
+
+module.exports = {
+  getMessages,
+  createMessage,
+};
