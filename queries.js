74508599ee8932cc1e03a1faedffdad79320e333
Matthew Sedlacek <sedlacek1991@gmail.com>
2020-12-11 14:44:30 -0800

    updates pool to client

--- queries.js
+++ queries.js
@@ -1,15 +1,34 @@
-const Pool = require("pg").Pool;
-const pool = new Pool({
-    // dbname="d6hadta3u0um4g", 
-    host="ec2-54-208-233-243.compute-1.amazonaws.com",
-    port=5432,
-    user="ynxfswvevbsccw", 
-    password="c92dc1792beb6c25c822e0211f7689af8f641965691ae720b46194b6e9531030", 
-    // sslmode=require
+// const Pool = require("pg").Pool;
+// const pool = new Pool({
+//     // dbname="d6hadta3u0um4g",
+//     host="ec2-54-208-233-243.compute-1.amazonaws.com",
+//     port=5432,
+//     user="ynxfswvevbsccw",
+//     password="c92dc1792beb6c25c822e0211f7689af8f641965691ae720b46194b6e9531030",
+//     // sslmode=require
+// });
+
+const { Client } = require("pg");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+client.connect();
+// client.query(
+//   "SELECT table_schema,table_name FROM information_schema.tables;",
+//   (err, res) => {
+//     if (err) throw err;
+//     for (let row of res.rows) {
+//       console.log(JSON.stringify(row));
+//     }
+//     client.end();
+//   }
+// );
 
 const getMessages = (request, response) => {
-  pool.query("SELECT * FROM messages ORDER BY id ASC", (error, results) => {
+  client.query("SELECT * FROM messages ORDER BY id ASC", (error, results) => {
     if (error) {
       throw error;
     }
@@ -20,7 +39,7 @@ const getMessages = (request, response) => {
 const createMessage = (request, response) => {
   const { text, username } = request.body;
 
-  pool.query(
+  client.query(
     "INSERT INTO messages (text, username) VALUES ($1, $2)",
     [text, username],
     (error, results) => {
